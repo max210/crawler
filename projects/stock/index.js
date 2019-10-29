e6980ec07175c384e2c2e7ed2bdda10e6e348e00
maximilian <mengxinnian@gmail.com>
2019-10-29 10:29:51 +0800

    stock

--- projects/stock/index.js
+++ projects/stock/index.js
@@ -1,6 +1,7 @@
 // 爬取股价
 
 const puppeteer = require('puppeteer')
+const schedule = require('node-schedule')
 
 const URL = 'http://quote.cfi.cn/'
 const companyList = [
@@ -20,19 +21,22 @@ const companyList = [
     targetPrice: 43
   }
 ]
+const scheduleJobRule = { hour: 14, minute: 0, second: 0 }
+const MAX_RETRY_TIME = 2
+let retryTime = 0
 
 async function task() {
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.goto(URL, { waitUntil: 'networkidle2' })
-    await page.waitFor('#searchcode')
+    await page.waitForSelector('#searchcode', { timeout: 60000, visible: true })
 
     let result = ''
     for (let i = 0; i < companyList.length; i++) {
       await page.type('#searchcode', companyList[i].id, { delay: 100 })
       await page.click('#submit')
-      await page.waitFor('#last')
+      await page.waitForSelector('#last', { timeout: 60000, visible: true })
       const price = await page.$eval('#last', el => el.innerText)
       result += `
         ${companyList[i].name}（${companyList[i].targetPrice}） 现价：${price && price.slice(0, -1)}
@@ -41,9 +45,15 @@ async function task() {
 
     console.log(result)
     await browser.close()  //关闭browser实例
+    retryTime = 0
   } catch (e) {
-    console.log('err', e)
+    if (retryTime < MAX_RETRY_TIME) {
+      retryTime += 1
+      task()
+    } else {
+      console.log('err', e)
+    }
   }
 }
 
-task()
+schedule.scheduleJob(scheduleJobRule, task)
