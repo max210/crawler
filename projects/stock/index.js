6a58d5b1003354fe2bc441bfccdcedf89e37e703
maximilian <mengxinnian@gmail.com>
2019-11-18 18:47:54 +0800

    add indexPrice

--- projects/stock/index.js
+++ projects/stock/index.js
@@ -4,7 +4,8 @@ const puppeteer = require('puppeteer')
 const schedule = require('node-schedule')
 const axios = require('axios')
 
-const URL = 'http://quote.cfi.cn/'
+const PRICE_URL = 'http://quote.cfi.cn/'
+const INDEX_URL = 'http://finance.sina.com.cn/realstock/company/sh000001/nc.shtml'
 const NOTICE_URL = 'https://oapi.dingtalk.com/robot/send?access_token=2d15d060f3251d9b25de2c4dae05be3ca6ec4a11718a0cc048490f127cde3e00'
 const companyList = [
   {
@@ -41,7 +42,7 @@ async function task() {
   try {
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
     const page = await browser.newPage()
-    await page.goto(URL, { waitUntil: 'networkidle2' })
+    await page.goto(PRICE_URL, { waitUntil: 'networkidle2' })
     await page.waitForSelector('#searchcode', { timeout: 60000, visible: true })
 
     let result = ''
@@ -54,12 +55,22 @@ async function task() {
 ${companyList[i].name}${companyList[i].targetPrice}  现价: ${price && price.slice(0, -1)}`
     }
 
+    const page1 = await browser.newPage()
+    await page1.goto(INDEX_URL, { waitUntil: 'networkidle2' })
+    await page1.waitForSelector('#price', { timeout: 60000, visible: true })
+    await page1.waitForSelector('#changeP', { timeout: 60000, visible: true })
+    const indexPrice = await page1.$eval('#price', el => el.innerText)
+    const upOrDown = await page1.$eval('#changeP', el => el.innerText)
+    result = `
+上证：${indexPrice} ${upOrDown.slice(0, 1)} ${result}`
+
     sendMessage(result)
     await browser.close()  //关闭browser实例
     retryTime = 0
   } catch (e) {
     if (retryTime < MAX_RETRY_TIME) {
       retryTime += 1
+      await browser.close()
       task()
     } else {
       sendMessage('获取失败')
